55e26af6c7f4ed76621010589d387b49e92d897b
justgo13 <jgao2299@gmail.com>
2022-11-24 20:36:28 -0500

    move text to update once

--- TheEthicalHackerCup/Assets/DefenseMenu/Scripts/AttackAttemptText.cs
+++ TheEthicalHackerCup/Assets/DefenseMenu/Scripts/AttackAttemptText.cs
@@ -21,20 +21,6 @@ public class AttackAttemptText : MonoBehaviour
         setupColors();
         GameManager.Instance = (GameManager)ScriptableObject.CreateInstance("GameManager");
         attemptText = this.gameObject.GetComponent<TextMeshProUGUI>();
-    }
-
-    
-    private void setupColors() {
-        // red 181, 0, 21
-        red = new Color(200f/255f, 0, 21f/255f);
-
-        // green 0, 189, 63
-        green = new Color(0, 189f/255f, 63f/255f);
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
         attemptCompleted = GameManager.Instance.GetAttackMinigamesAttempted(sc);
         attemptsTotal = GameManager.Instance.GetDefenseUpgradeLevels(sc) + 1;
         string attempts = attackAttemptText + "\n" + attemptCompleted + "/" + attemptsTotal;
@@ -42,7 +28,23 @@ public class AttackAttemptText : MonoBehaviour
         attemptText.color = isAttemptComplete() ? green : red;
     }
 
-    public bool isAttemptComplete() {
+
+    private void setupColors()
+    {
+        // red 181, 0, 21
+        red = new Color(200f / 255f, 0, 21f / 255f);
+
+        // green 0, 189, 63
+        green = new Color(0, 189f / 255f, 63f / 255f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public bool isAttemptComplete()
+    {
         // checks if completed attack attempts equal to total attack attempts
         return attemptCompleted == attemptsTotal;
     }
