e17f71b238fd6226295ae6c2a179db61a17f900d
DavidHaighton <DavidHaighton@cmail.carleton.ca>
2023-01-08 19:12:37 -0500

    refactored and added info panels

--- TheEthicalHackerCup/Assets/EditModeTests/QuizUnit.cs
+++ TheEthicalHackerCup/Assets/EditModeTests/QuizUnit.cs
@@ -11,11 +11,12 @@ public class TestQuizState
 {
 
 
-
     [Test]
     public void DivideByZero()
     {
-        var quiz = new QuizState("A", new List<QuestionState>());
+        var slides = new List<Slide>() { };
+        slides.Add(new Slide("A", new InfoContent("B")));
+        var quiz = new Quiz(slides);
         Assert.DoesNotThrow(() => { var b = quiz.Percent; });
         Assert.DoesNotThrow(() => { var a = quiz.IsPassingScore; });
 
@@ -24,49 +25,117 @@ public class TestQuizState
     [Test]
     public void TestBadScore()
     {
-        var questions = new List<QuestionState>();
-        for (int i = 0; i < 10; i++)
+        var slides = new List<Slide>();
         {
-            var state = new CheckboxState();
-            state.Selected.Add(1);
-            state.CorrectOptions.Add(0);
-            questions.Add(state);
+            var info = new InfoContent("Welcome");
+            var s = new Slide("SLIDE 1", info);
+            slides.Add(s);
         }
-        var quiz = new QuizState("TestQuiz", questions);
-        Assert.False(quiz.IsPassingScore);
-        Assert.False(quiz.IsNoMoreQuestions);
+        {
+            var cq = new CheckBox(new HashSet<int>() { }, new List<string>() { "Hello", "Bruh" }, new HashSet<int>() { 1 });
+            var s = new Slide("SLIDE 2", cq);
+            slides.Add(s);
+        }
+        {
+            var sq = new RadioBox(1, new List<string>() { "Hello", "Bruh" }, 0);
+            var s = new Slide("SLIDE 3", sq);
+            slides.Add(s);
+        }
+        var q = new Quiz(slides);
+
+        var model = new QuizModel(q);
+        model.QuizClosed += delegate (object sender, QuizClosedEvent e) { };
+        model.QuizSubmitted += delegate (object sender, QuizSubmittedEvent e) { };
+        model.SlideChanged += delegate (object sender, NextSlideEvent e) { };
+
+        model.start();
+        model.next();
+        model.next();
+        model.next();
+        Assert.IsTrue(q.IsNoMoreQuestions); Assert.AreEqual(0, q.Percent);
+        Assert.IsFalse(q.IsPassingScore);
+        
     }
     [Test]
     public void TestGoodScore()
     {
-        var questions = new List<QuestionState>();
-        for (int i = 0; i < 10; i++)
+        var slides = new List<Slide>();
         {
-            var state = new CheckboxState();
-            state.Selected = new HashSet<int>() { 1 };
-            state.CorrectOptions = new HashSet<int>() { 1 };
-            questions.Add(state);
-
+            var info = new InfoContent("Welcome");
+            var s = new Slide("SLIDE 1", info);
+            slides.Add(s);
         }
-        var quiz = new QuizState("TestQuiz", questions);
-        quiz.updateScore();
-        Assert.True(quiz.IsPassingScore);
-        Assert.False(quiz.IsNoMoreQuestions);
+        {
+            var cq = new CheckBox(new HashSet<int>() {1 }, new List<string>() { "Hello", "Bruh" }, new HashSet<int>() { 1 });
+            var s = new Slide("SLIDE 2", cq);
+            slides.Add(s);
+        }
+        {
+            var sq = new RadioBox(1, new List<string>() { "Hello", "Bruh" }, 0);
+            var s = new Slide("SLIDE 3", sq);
+            slides.Add(s);
+        }
+        var q = new Quiz(slides);
+
+        var model = new QuizModel(q);
+        model.QuizClosed += delegate (object sender, QuizClosedEvent e) { };
+        model.QuizSubmitted += delegate (object sender, QuizSubmittedEvent e) { };
+        model.SlideChanged += delegate (object sender, NextSlideEvent e) { };
+
+        model.start();
+        model.next();
+        model.next();
+        model.next();
+        Assert.IsTrue(q.IsNoMoreQuestions); Assert.AreEqual(50, q.Percent);
+        Assert.IsTrue(q.IsPassingScore);
+
+    }
+
+
+
+
+    [Test]
+    public void xml()
+    {
+        var slides = new List<Slide>();
+        {
+            var info = new InfoContent("Welcome");
+            var s = new Slide("SLIDE 1", info);
+            slides.Add(s);
+        }
+        {
+            var cq = new CheckBox(new HashSet<int>() { }, new List<string>() { "Hello", "Bruh" }, new HashSet<int>() { 1 });
+            var s = new Slide("SLIDE 2", cq);
+            slides.Add(s);
+        }
+        {
+            var sq = new RadioBox(1, new List<string>() { "Hello", "Bruh" }, 0);
+            var s = new Slide("SLIDE 3", sq);
+            slides.Add(s);
+        }
+        var q = new Quiz(slides);
+
+        var xml = q.ToXml();
+
+        var q2 = Quiz.FromXml(xml.ToString());
+
+        var xml2 = q2.ToXml();
+
+        Assert.AreEqual(xml.ToString(), xml2.ToString());
+
     }
 
 }
-
 //All transitions criterion
-public class TestQuiz
+public class TestQuizRunThroughs
 {
 
-    QuizState quizState;
-    RadioState q1;
-    CheckboxState q2;
+    Quiz quizState;
+    RadioBox q1;
+    CheckBox q2;
     IList<int> questionEndCount;
     IList<int> questionNotifyCount;
     int nextQuestionCount;
-    int startQuizCount;
     int submittedCount;
     int endQuizCount;
     QuizModel quiz;
@@ -75,31 +144,22 @@ public class TestQuiz
     {
         //reset counters
         nextQuestionCount = 0;
-        startQuizCount = 0;
         endQuizCount = 0;
         submittedCount = 0;
         //make stubs
         //setup questions
-        q1 = new RadioState();
-        q1.Name = "Question 1";
-        q1.CorrectOption = 0;
-        q1.Options = new List<string> { "Option 0", "Option 1", "Option 2" };
-        q2 = new CheckboxState();
-        q2.Name = "Quiestopm 2";
-        q2.CorrectOptions = new HashSet<int> { 1, 2 };
-        q2.Options = new List<string> { "A", "B", "C", "D" };
+        
+        q1 = new RadioBox(0, new List<string> { "Option 0", "Option 1", "Option 2" }, 0);
+        var s1 = new Slide("slide 1", q1);
+        
+        q2 = new CheckBox(new HashSet<int>(), new List<string> { "A", "B", "C", "D" }, new HashSet<int> { 1, 2 });
+        var s2 = new Slide("slide 2", q2);
         //make quiz state
-        quizState = new QuizState("MyQuiz", new List<QuestionState> { q1, q2 });
+        quizState = new Quiz(new List<Slide> { s1, s2 });
         //make quiz
         quiz = new QuizModel(quizState);
-        quiz.QuizStarted += delegate (object sender, QuizStartedEvent evt) {
-            if (evt.quizName == quizState.Name)
-            {
-                startQuizCount++;
-            }
-        };
 
-        quiz.QuestionChanged += delegate (object sender, NextQuestionEvent args) {
+        quiz.SlideChanged += delegate (object sender, NextSlideEvent args) {
             nextQuestionCount++;
         };
         quiz.QuizSubmitted += delegate (object sender, QuizSubmittedEvent args) {
@@ -115,12 +175,10 @@ public class TestQuiz
     public void TestRunThrough()
     {
         quiz.start(); //start the quiz
-        quiz.next(); //go into the first question
-        quiz.next(); //go into the second question
-        quiz.next(); //show results
+        quiz.next(); //go into the second
+        quiz.next(); //go into results
         quiz.next(); //submit
 
-        Assert.AreEqual(1, startQuizCount);
         Assert.AreEqual(2, nextQuestionCount);
         Assert.AreEqual(1, submittedCount);
         Assert.AreEqual(1, endQuizCount);
@@ -128,34 +186,7 @@ public class TestQuiz
     [Test]
     public void TestQuizLifeCycleNoQuestions()
     {
-        var qs2 = new QuizState("Empty", new List<QuestionState>());
-        var quiz2 = new QuizModel(qs2);
-
-        quiz2.QuizStarted += delegate (object sender, QuizStartedEvent evt) {
-            startQuizCount++;
-        };
-
-        quiz2.QuestionChanged += delegate (object sender, NextQuestionEvent args) {
-            nextQuestionCount++;
-        };
-        quiz2.QuizSubmitted += delegate (object sender, QuizSubmittedEvent args) {
-            submittedCount++;
-        };
-        quiz2.QuizClosed += delegate (object sender, QuizClosedEvent args) {
-            endQuizCount++;
-        };
-
-
-        quiz2.start();//start
-        quiz2.next();//submitted
-        quiz2.next();//closed
-
-
-        Assert.AreEqual(1, startQuizCount);
-        Assert.AreEqual(0, nextQuestionCount);
-        Assert.AreEqual(1, submittedCount);
-        Assert.AreEqual(1, endQuizCount);
-
+        Assert.Catch(() => { var qs2 = new Quiz(new List<Slide>()); });
+        
     }
-
 }
