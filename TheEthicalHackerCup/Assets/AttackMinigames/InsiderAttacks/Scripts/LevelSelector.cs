79a06f5d96def6db469a68695babb84657a6cf5d
Jason Gao <jgao2299@gmail.com>
2023-01-17 15:32:43 -0500

    dynamic generate level

--- TheEthicalHackerCup/Assets/AttackMinigames/InsiderAttacks/Scripts/LevelSelector.cs
+++ TheEthicalHackerCup/Assets/AttackMinigames/InsiderAttacks/Scripts/LevelSelector.cs
@@ -4,10 +4,20 @@ using UnityEngine;
 
 public class LevelSelector : MonoBehaviour
 {
+    [SerializeField]
+    private GameObject[] levels;
     // Start is called before the first frame update
     void Start()
     {
         InsiderSingleton.GetInstance().PickLevel();
+        string levelName = InsiderSingleton.GetInstance().GetCurrentLevelName();
+        GameObject level = null;
+        foreach (GameObject g in levels) {
+            if (g.name.Equals(levelName)) {
+                level = g;
+            }
+        }
+        Instantiate(level, transform);
     }
 
     // Update is called once per frame
