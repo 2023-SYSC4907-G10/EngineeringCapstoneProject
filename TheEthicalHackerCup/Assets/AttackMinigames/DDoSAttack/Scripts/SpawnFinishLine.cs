1e42e2ce138392a65692b913e2c39a5ad2b36add
mack-wallace <mackwallacebusiness@gmail.com>
2023-01-24 11:31:18 -0500

    fixed spawn settings + length of finish line

--- TheEthicalHackerCup/Assets/AttackMinigames/DDoSAttack/Scripts/SpawnFinishLine.cs
+++ TheEthicalHackerCup/Assets/AttackMinigames/DDoSAttack/Scripts/SpawnFinishLine.cs
@@ -5,17 +5,25 @@ using UnityEngine;
 public class SpawnFinishLine : MonoBehaviour
 {
     public GameObject finishLine;
-    public float spawnTime;
+    public int frameUntilSpawn;
+    private int startFrame;
+
+    void Start()
+    {
+        startFrame = Time.frameCount;
+    }
 
     // Update is called once per frame
     void Update()
     {
-        if(Time.fixedTime == spawnTime){
+        if(Time.fixedTime == startFrame + frameUntilSpawn)
+        {
             Spawn();
         }
     }
 
-    void Spawn(){
+    void Spawn()
+    {
         Instantiate(finishLine, transform.position + new Vector3(1, 1 , 0), transform.rotation);
     }
 
