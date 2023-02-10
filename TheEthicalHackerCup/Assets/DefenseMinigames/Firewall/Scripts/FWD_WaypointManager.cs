f6a77301dda1e9571b335f166a4a4d2a80f957d5
Millan <real.millan.wang@gmail.com>
2023-02-10 13:10:12 -0500

    Setup directories and created waypoint management scripts

---
+++ TheEthicalHackerCup/Assets/DefenseMinigames/Firewall/Scripts/FWD_WaypointManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FWD_WaypointManager : MonoBehaviour
+{
+    public FWD_Waypoint rootWaypoint;
+    public List<FWD_Waypoint> secondWaypoints; //Targets 1,2 and junction to 3,4,5
+    public List<FWD_Waypoint> thirdWaypoints; //Targets 3,4,5
+
+
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public List<FWD_Waypoint> GetWaypointPath()
+    {
+        // Every packet will get a 3 waypoint path. If they arrive at T1/T2, then they will be deleted and won't even nav to the 3rd waypoint
+        List<FWD_Waypoint> waypoints = new List<FWD_Waypoint>();
+
+        waypoints.Add(rootWaypoint);
+        waypoints.Add(secondWaypoints[Random.Range(0, secondWaypoints.Count)]);
+        waypoints.Add(thirdWaypoints[Random.Range(0, thirdWaypoints.Count)]);
+
+        return waypoints;
+    }
+}
